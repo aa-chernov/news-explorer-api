703293a166dabc7af2339fc2ac7feac572d776c8
Alex Chernov <chernov.email@yandex.ru>
2020-08-10 02:05:09 +0300

    Добавлен app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const helmet = require('helmet');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const articlesPath = require('./routes/articles');
+const usersPath = require('./routes/users');
+const resourcePath = require('./routes/resource');
+const serverError = require('./middlewares/serverError');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/news-explorer', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(requestLogger);
+
+// ------УДАЛИТЬ ПОСЛЕ ТЕСТИРОВАНИЯ------
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+// ------УДАЛИТЬ ПОСЛЕ ТЕСТИРОВАНИЯ------
+
+app.use('/', articlesPath);
+app.use('/', usersPath);
+app.use('/', resourcePath);
+app.use(errorLogger);
+app.use(errors());
+app.use('/', serverError);
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Слушаем порт: ${PORT}`);
+});
