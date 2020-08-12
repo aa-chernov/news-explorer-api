f40b01fb37dd510eb87e572a5f787a74cfd1382f
Alex Chernov <chernov.email@yandex.ru>
2020-08-12 23:23:20 +0300

    Переписано удаление статьи пользователем

--- controllers/articles.js
+++ controllers/articles.js
@@ -49,18 +49,17 @@ module.exports.createArticle = (req, res, next) => {
 };
 
 module.exports.deleteArticle = (req, res, next) => {
-  Article.findById(req.params)
+  Article.findById(req.params._id)
+    .select('+owner')
+    .orFail(new NotFoundError(constants.NO_ARTICLE))
     .then((article) => {
-      if (!article) {
-        next(new NotFoundError(constants.NO_ARTICLE));
-      } else if (article.owner.toString() !== req.user._id) {
-        next(new ForbiddenError(constants.NO_RIGHTS));
+      if (JSON.stringify(article.owner) !== JSON.stringify(req.user._id)) {
+        throw new ForbiddenError(constants.NO_RIGHTS);
       }
-      return Article.findByIdAndRemove(req.params._id)
-        .then((articleForDel) => {
-          res.send({ message: `Удалена статья "${articleForDel.title}" с id ${articleForDel._id}` });
-        })
-        .catch(next);
+      Article.deleteOne(article)
+        .then(() => res.send({
+          message: `Удалена статья: "${article.title}"`,
+        }));
     })
     .catch(next);
 };
