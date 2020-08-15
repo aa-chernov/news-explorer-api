fe3b4c7da9ad422b1c942979a263f7f06b28447d
Alex Chernov <chernov.email@yandex.ru>
2020-08-15 17:33:35 +0300

    Обновлено максимальное количество допустимых запросов

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 500,
   message: 'Число запросов с одного IP ограничено. Сделайте новый запрос через 15 минут',
 });
 
