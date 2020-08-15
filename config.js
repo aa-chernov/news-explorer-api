bf99837ccefd3ac81e051f0a8f2e1c9415e7659b
Alex Chernov <chernov.email@yandex.ru>
2020-08-15 20:50:40 +0300

    Отладка

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 
 const {
-  PORT = 3000,
+  PORT,
   HOST,
   NODE_ENV,
   JWT_SECRET,
 } = process.env;
 
-module.exports = {
-  PORT,
-  HOST: NODE_ENV === 'production' ? HOST : 'mongodb://localhost:27017/news-explorer',
-  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'super-puper-dev-secret',
-};
+module.exports.PORT = PORT || 3000;
+module.exports.HOST = NODE_ENV === 'production' ? HOST : 'mongodb://localhost:27017/news-explorer';
+module.exports.JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'super-puper-dev-secret';
