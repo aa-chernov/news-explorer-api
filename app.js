631d879af08e7fd1cf1296fcd15261262c77b1e1
Alex Chernov <chernov.email@yandex.ru>
2020-08-15 23:54:54 +0300

    Отладка

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const serverError = require('./middlewares/serverError');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT } = require('./config');
+const { PORT, HOST } = require('./config');
 
 const app = express();
 
-// mongoose.connect(HOST, {
-mongoose.connect('mongodb://localhost:27017/news-explorer', {
+mongoose.connect(HOST, {
+// mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
