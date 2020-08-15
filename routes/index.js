6a5890851ed292c86163cb14339dbde076160b52
Alex Chernov <chernov.email@yandex.ru>
2020-08-15 13:25:05 +0300

    Добавлен единый роут index.js

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const articlesRouter = require('./articles');
+const usersRouter = require('./users');
+const resourceRouter = require('./resource');
+
+module.exports = { articlesRouter, usersRouter, resourceRouter };
--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,21 @@ const { getUser, createUser, login } = require('../controllers/users');
 const constants = require('../constants');
 
 usersRouter
-  .post('/signin', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }), login)
-  .post('/signup', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-      name: Joi.string().required().max(30).error(() => new BadRequestError(constants.TOO_SHORT)),
-    }),
-  }), createUser);
+  .post('/signin',
+    celebrate({
+      body: Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8),
+      }),
+    }), login)
+  .post('/signup',
+    celebrate({
+      body: Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8),
+        name: Joi.string().required().max(30).error(() => new BadRequestError(constants.TOO_SHORT)),
+      }),
+    }), createUser);
 usersRouter
   .get('/users/me', auth, getUser);
 
