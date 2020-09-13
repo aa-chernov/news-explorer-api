71b318c37a67d8757a03cb5ef708bf64df6c6d0c
Alex Chernov <chernov.email@yandex.ru>
2020-09-13 18:22:38 +0300

    fix

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ const serverError = require('./middlewares/serverError');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const whitelist = ['http://localhost:8080/', 'http://localhost:3000/', 'https://aa-chernov.github.io/news-explorer-frontend/pages/index.html#',
-//   'https://aa-chernov.github.io/', 'https://api.news-explorers.tk/', 'https://news-explorers.tk/', 'http://api.news-explorers.tk', 'http://news-explorers.tk/'];
+const whitelist = ['http://localhost:8080/', 'http://localhost:3000/', 'https://aa-chernov.github.io/news-explorer-frontend/pages/index.html#',
+  'https://aa-chernov.github.io/', 'https://api.news-explorers.tk/', 'https://news-explorers.tk/', 'http://api.news-explorers.tk', 'http://news-explorers.tk/'];
 
-// const corsOptions = {
-//   origin(origin, callback) {
-//     console.log(origin);
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin(origin, callback) {
+    console.log(origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 const { PORT } = require('./config');
 
@@ -39,8 +39,8 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(requestLogger);
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use('/', articlesRouter);
 app.use('/', usersRouter);
 app.use('/', resourceRouter);
